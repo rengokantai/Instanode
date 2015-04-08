18d2b977889eb0f8da3a20283b176fb7ff03e733
Hernan Y.Ke <yuriqiao@gmail.com>
2015-04-08 08:39:01 +0800

    Add head.ejs and solve index.ejs issue

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 /**
  * Created by Hernan Y.Ke on 4/7/15.
  */
-var express=require("express");
+var express=require('express');
 var app=express();
-var instagram=require("instagram-node").instagram();
+var instagram=require('instagram-node').instagram();
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine','ejs');
 
+
+instagram.use({
+    client_id:'e20bb5f5169f41e49e9161054e20b02d',
+    client_secret:'f16a448782d74a72999e681f0fecbaa8'
+});
+
+
+
+app.get('/',function(req,res){
+    instagram.media_popular(function(err,medias,remaininglimit){
+        res.render('pages/index',{grams:medias})
+    })
+})
 
 app.listen(8080,function(err){
     if(err){
